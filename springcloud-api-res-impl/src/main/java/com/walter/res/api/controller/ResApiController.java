1abe24bc7292634f1b6226a63a01f6e9695eb98a
waltertan1988 <waltertan1988@163.com>
2019-01-19 23:25:59 +0800

    设置Hystrix超时时间

--- springcloud-api-res-impl/src/main/java/com/walter/res/api/controller/ResApiController.java
+++ springcloud-api-res-impl/src/main/java/com/walter/res/api/controller/ResApiController.java
@@ -2,6 +2,7 @@ package com.walter.res.api.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,11 @@ public class ResApiController extends BaseResApiController implements ResApi {
 	
 	@Override
 	public List<JpaSysMenu> listMenu(@PathVariable("username")String username) {
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return new ArrayList<JpaSysMenu>();
 	}
 }
