717b2395973db31a50a27c17770f833c2ce72587
waltertan1988 <waltertan1988@163.com>
2019-01-20 18:19:59 +0800

    开启zuul，修正zuul端口

--- springcloud-demo-zuul/src/main/java/com/walter/ZuulApplication.java
+++ springcloud-demo-zuul/src/main/java/com/walter/ZuulApplication.java
@@ -2,12 +2,12 @@ package com.walter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages={"com.walter", "org.walter"})
 @EnableFeignClients(basePackages="com.walter")
-@EnableCircuitBreaker
+@EnableZuulProxy
 public class ZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
