fff3eed003a537a22573b43f47c9b0649292c935
waltertan <waltertan1988@163.com>
2019-04-28 01:22:01 +0800

    排版

--- springcloud-base-service/src/main/java/org/walter/base/security/WebSecurityConfig.java
+++ springcloud-base-service/src/main/java/org/walter/base/security/WebSecurityConfig.java
@@ -46,14 +46,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return jdbcTokenRepositoryImpl;
 	}
 
-	// 用户认证
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
 	}
 
-	// 请求授权
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		if(customeSecurityProperties.getHttpBasicEnable()) {
@@ -85,10 +83,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.userDetailsService(userDetailsService)
 				.and()
 			.authorizeRequests()
-			.antMatchers(HttpMethod.GET, LOGIN_PAGE_DECISION_URL).permitAll()
-			.antMatchers("/admin/**").authenticated()
-			.and()
-			.csrf().disable();
+				.antMatchers(HttpMethod.GET, LOGIN_PAGE_DECISION_URL).permitAll()
+				.antMatchers("/admin/**").authenticated()
+				.and()
+			.csrf()
+				.disable();
 	}
 	
 	private final String LOGIN_PAGE_DECISION_URL = "/loginPageDecision";
