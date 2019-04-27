ac1c3092d22e0297a0183d8fda7806733edf56b3
waltertan <waltertan1988@163.com>
2019-04-27 19:42:35 +0800

    使用SavedRequestAwareAuthenticationSuccessHandler代替AuthenticationSuccessHandler

--- springcloud-base-service/src/main/java/org/walter/base/security/authenticate/CustomAuthenticationSuccessHandler.java
+++ springcloud-base-service/src/main/java/org/walter/base/security/authenticate/CustomAuthenticationSuccessHandler.java
@@ -1,39 +1,32 @@
 package org.walter.base.security.authenticate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
-@Slf4j
-public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
-	private RequestCache requestCache = new HttpSessionRequestCache();
-	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-	
-	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 
-		SavedRequest savedRequest = requestCache.getRequest(request, response);
-		if(savedRequest != null && StringUtils.isEmpty(request.getHeader("X-Requested-With"))) {
-			log.info("引发重定向到登录页面的请求：{}", savedRequest.getRedirectUrl());
+		if(StringUtils.isEmpty(request.getHeader("X-Requested-With"))) {
 			// 非ajax请求，重定向到之前的页面
-			redirectStrategy.sendRedirect(request, response, savedRequest.getRedirectUrl());
+			super.onAuthenticationSuccess(request, response, authentication);
+		}else {
+			PrintWriter out = response.getWriter();
+			out.print(new ObjectMapper().writeValueAsString("SUCCESS"));
+			out.flush();
 		}
 	}
 }
