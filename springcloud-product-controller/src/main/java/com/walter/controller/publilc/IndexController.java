5f70515278a9351826c0db4274a27ff1a9f19feb
waltertan <waltertan1988@163.com>
2019-05-03 18:17:17 +0800

    添加product-controller模块

---
+++ springcloud-product-controller/src/main/java/com/walter/controller/publilc/IndexController.java
@@ -0,0 +1,17 @@
+package com.walter.controller.publilc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class IndexController {
+	
+	@GetMapping("/index")
+	@ResponseBody
+	public String index() {
+		return "Hello Index";
+	}
+}
