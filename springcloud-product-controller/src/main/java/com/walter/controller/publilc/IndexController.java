ac5961e10aaf0c987bd40bb2bc2e5c5c5a3f08ce
waltertan <waltertan1988@163.com>
2019-05-03 20:32:11 +0800

    添加springcloud-website模块

--- springcloud-product-controller/src/main/java/com/walter/controller/publilc/IndexController.java
+++ springcloud-product-controller/src/main/java/com/walter/controller/publilc/IndexController.java
@@ -2,6 +2,7 @@ package com.walter.controller.publilc;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class IndexController {
 	
 	@GetMapping("/index")
 	@ResponseBody
-	public String index() {
-		return "Hello Index";
+	public String index(@RequestParam String helloTo) {
+		return "Hello " + helloTo;
 	}
 }
